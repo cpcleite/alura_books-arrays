d4cf3e06db3d6f88da57a1125511ad4657505e85
Celso Leite <cpcleite@me.com>
2023-02-25 18:07:22 -0300

    Inicial

---
+++ app/metodoFilter.js
@@ -0,0 +1,30 @@
+const botoes = document.querySelectorAll('.btn');
+botoes.forEach(btn => btn.addEventListener('click', filtrarLivros));
+
+function filtrarLivros() {
+    const categoria = this.value
+
+    let livrosFiltrados = categoria == 'disponivel' ? filtrarPorDisponibilidade() : filtrarPorCategoria(categoria);
+    exibirOsLivrosNaTela(livrosFiltrados);
+    if (categoria == 'disponivel') {
+        const valorTotal = calcularValorTotalDeLivrosDisponiveis(livrosFiltrados);
+        exibirValorTotalDosLivrosDisponiveisNaTela(valorTotal);
+    }
+}
+
+function filtrarPorCategoria(categoria) {
+    return livros.filter(livro => livro.categoria == categoria);
+}
+
+function filtrarPorDisponibilidade() {
+    return livros.filter(livro => livro.quantidade > 0);
+}
+
+function exibirValorTotalDosLivrosDisponiveisNaTela(valor) {
+    elementoComValorTotalDeLivrosDisponiveis.innerHTML = `
+    <div class="livros__disponiveis">
+      <p>Todos os livros disponíveis por R$ <span id="valor">${valor}</span></p>
+    </div>
+    `
+}
+
